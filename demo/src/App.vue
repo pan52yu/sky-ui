3212164bc6e32e1bfc83b7fb1dac1f3a3d2d8ca6
huyu <yhu8836@gmail.com>
2023-10-18 10:15:28 +0800

    搭建 demo 测试效果

---
+++ demo/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// demo/src/App.vue
+import { Button, Input } from "@skyui/ui";
+</script>
+
+<template>
+  <div>
+    123
+    <Button>1111</Button>
+    <Input />
+  </div>
+</template>
