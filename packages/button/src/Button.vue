d9df4ffadd67e12818068faf609a8a3b9e78d766
huyu <yhu8836@gmail.com>
2023-10-18 11:22:27 +0800

    集成Stylelint

--- packages/button/src/Button.vue
+++ packages/button/src/Button.vue
@@ -25,11 +25,27 @@ function clickHandler() {
   </button>
 </template>
 
-<style lang="scss" scoped>
-.openx-button {
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 8px 16px;
-  margin: 0 8px;
+<style lang="scss">
+.testClass {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5;
+  color: #212529;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  user-select: none;
+  background-color: #e9ecef;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  transition:
+    color 0.15s ease-in-out,
+ background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out,
+ box-shadow 0.15s ease-in-out;
 }
 </style>
