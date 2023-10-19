f1f9732ec5a6c06bbaab32a55aa55c5d4890f69b
huyu <yhu8836@gmail.com>
2023-10-19 10:27:34 +0800

    feat: demo展示

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,12 +1,101 @@
 <script setup lang="ts">
 // demo/src/App.vue
-import { Button, Input } from '@skyui/ui';
+import { ref, reactive } from 'vue';
+import {
+  Button,
+  Input,
+  ConfigProvider,
+  useTheme,
+  tinyThemeVars,
+  themeVars,
+  SkyuiCssVarsConfig,
+} from '@skyui/ui';
+
+const { setTheme } = useTheme();
+
+const currentGlobalTheme = ref<'default' | 'tiny'>('default');
+
+// 全局主题切换
+function switchGlobalTheme() {
+  if (currentGlobalTheme.value === 'tiny') {
+    currentGlobalTheme.value = 'default';
+    setTheme(themeVars);
+  } else {
+    currentGlobalTheme.value = 'tiny';
+    setTheme(tinyThemeVars);
+  }
+}
+
+const currentSecondLineTheme = ref<'default' | 'tiny'>('default');
+const secondLineThemeVars: SkyuiCssVarsConfig = reactive({});
+// 局部主题切换
+function switchSecondLineTheme() {
+  if (currentSecondLineTheme.value === 'tiny') {
+    currentSecondLineTheme.value = 'default';
+    Object.assign(secondLineThemeVars, themeVars);
+  } else {
+    currentSecondLineTheme.value = 'tiny';
+    Object.assign(secondLineThemeVars, tinyThemeVars);
+  }
+}
 </script>
 
 <template>
   <div>
+    <span
+      class="i-op-alert-marked inline-block"
+    />
     123
-    <Button>1111</Button>
+    <!-- 第一组 button 省略 。。。 -->
+    <ConfigProvider
+      class="btns"
+      :theme-vars="secondLineThemeVars"
+    >
+      <Button plain>
+        Button
+      </Button>
+      <Button
+        type="primary"
+        plain
+      >
+        Button
+      </Button>
+      <Button
+        type="success"
+        plain
+      >
+        Button
+      </Button>
+      <Button
+        type="danger"
+        plain
+      >
+        Button
+      </Button>
+      <Button
+        type="warning"
+        plain
+      >
+        Button
+      </Button>
+      <Button
+        type="info"
+        plain
+      >
+        Button
+      </Button>
+    </ConfigProvider>
+    <!-- 第三组 button 省略 。。。 -->
+    <div class="btns">
+      <Button @click="switchGlobalTheme">
+        切换全局主题，当前：{{ currentGlobalTheme }}
+      </Button>
+      <Button @click="switchSecondLineTheme">
+        切换第二行主题，当前：{{ currentSecondLineTheme }}
+      </Button>
+    </div>
     <Input />
   </div>
 </template>
+
+<!-- 省略样式定义 。。。 -->
--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,5 +1,6 @@
 // demo/src/main.ts
 import { createApp } from 'vue';
 import App from './App.vue';
+import 'virtual:uno.css';
 
 createApp(App).mount('#app');
