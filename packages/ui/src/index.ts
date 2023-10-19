54b318ecac9c08b74bc1d351eb3e80d08035068d
huyu <yhu8836@gmail.com>
2023-10-19 10:26:44 +0800

    feat: 组件库主包构建

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,6 @@
 // packages/ui/src/index.ts
 export * from '@skyui/button';
 export * from '@skyui/input';
+export * from '@skyui/config-provider';
 export * from '@skyui/shared';
+export * from '@skyui/styles';
