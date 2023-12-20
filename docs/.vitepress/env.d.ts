ad7fd8fc5b61f1db5374225e6964f8bf7c86dd20
huyu <yhu8836@gmail.com>
2023-12-20 16:31:35 +0800

    fix: fix bug

--- docs/.vitepress/env.d.ts
+++ docs/.vitepress/env.d.ts
@@ -1,7 +1,8 @@
-// docs/.vitepress/env.d.ts
-declare module 'markdown-it-container' {
-    import type {PluginWithParams} from 'markdown-it';
+/// <reference types="vite/client" />
 
-    const container: PluginWithParams;
-    export = container;
+declare module 'markdown-it-container' {
+  import type { PluginWithParams } from 'markdown-it';
+
+  const container: PluginWithParams;
+  export = container;
 }
